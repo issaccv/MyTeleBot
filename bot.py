c0bf1f6ec618b4477118178acbc3cd115a18c449
issacc <8qllyhy@gmail.com>
2020-03-23 12:12:16 +0800

    change to webhook behind Nginx

--- bot.py
+++ bot.py
@@ -1,13 +1,37 @@
-import telebot
+import time
+
+import flask
 import requests
-import config
+import telebot
 from telebot import apihelper
 
-TOKEN = config.TOKEN
+import config
 
+TOKEN = config.TOKEN
+WEBHOOK_HOST = 'https://unlock.issac.bid'
+WEBHOOK_PORT = 443
+WEBHOOK_LISTEN = '0.0.0.0'
+
+WEBHOOK_SSL_CERT = ''
+WEBHOOK_SSL_PRIV = ''
+
+WEBHOOK_URL_BASE = 'https://{}:{}'.format(WEBHOOK_HOST, WEBHOOK_PORT)
+WEBHOOK_URL_PATH = '/{}/'.format(TOKEN)
 # apihelper.proxy = {'https':'socks5://127.0.0.1:10808'}
 
 bot = telebot.TeleBot(TOKEN)
+
+app = flask.Flask(__name__)
+
+@app.route(WEBHOOK_URL_PATH, methods=['POST'])
+def webhook():
+    if flask.request.headers.get('content-type') == 'application/json':
+        json_string = flask.request.get_data().decode('utf-8')
+        update = telebot.types.Update.de_json(json_string)
+        bot.process_new_updates([update])
+        return ''
+    else:
+        flask.abort(403)
 
 headers = {
         'referer': 'https://nmsl.shadiao.app/?from_chp',
@@ -34,4 +58,15 @@ def random_zuan(message):
     msg = get_zuan({'lang': 'zh_cn'})
     bot.send_message(message.chat.id, msg.text)
 
-bot.polling()
+bot.remove_webhook()
+
+time.sleep(1)
+
+bot.set_webhook(
+    url=WEBHOOK_URL_BASE + WEBHOOK_URL_PATH
+)
+
+app.run(
+    host='127.0.0.1',
+    port=5000
+)
