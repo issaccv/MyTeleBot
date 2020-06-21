9eb18fb77586d4a8b36cdff8a67df874ef5f5607
issacc <8qllyhy@gmail.com>
2020-06-21 10:16:10 +0800

    style(bot,config): 更新样式,同时将部分config转移

--- bot.py
+++ bot.py
@@ -8,95 +8,95 @@ from telebot import apihelper
 import config
 
 TOKEN = config.TOKEN
-IP_TOKEN = config.IP_TOKEN    
-WEBHOOK_HOST = 'https://unlock.issacc.bid'
-WEBHOOK_PORT = 443
-WEBHOOK_LISTEN = '0.0.0.0'
+IP_TOKEN = config.IP_TOKEN
 
-WEBHOOK_SSL_CERT = ''
-WEBHOOK_SSL_PRIV = ''
 
-WEBHOOK_URL_BASE = 'https://{}:{}'.format(WEBHOOK_HOST, WEBHOOK_PORT)
-WEBHOOK_URL_PATH = '/{}/'.format(TOKEN)
+WEBHOOK_SSL_CERT = ""
+WEBHOOK_SSL_PRIV = ""
+
+WEBHOOK_URL_BASE = "https://{}:{}".format(config.WEBHOOK_HOST, config.WEBHOOK_PORT)
+WEBHOOK_URL_PATH = "/{}/".format(TOKEN)
 # apihelper.proxy = {'https':'socks5://127.0.0.1:10808'}
 
 bot = telebot.TeleBot(TOKEN)
 
 app = flask.Flask(__name__)
 
-@app.route(WEBHOOK_URL_PATH, methods=['POST'])
+
+@app.route(WEBHOOK_URL_PATH, methods=["POST"])
 def webhook():
-    if flask.request.headers.get('content-type') == 'application/json':
-        json_string = flask.request.get_data().decode('utf-8')
+    if flask.request.headers.get("content-type") == "application/json":
+        json_string = flask.request.get_data().decode("utf-8")
         update = telebot.types.Update.de_json(json_string)
         bot.process_new_updates([update])
-        return ''
+        return ""
     else:
         flask.abort(403)
 
+
 headers = {
-        'referer': 'https://nmsl.shadiao.app/?from_chp',
-        'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.132 Safari/537.36'
-    }
+    "referer": "https://nmsl.shadiao.app/?from_chp",
+    "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.132 Safari/537.36",
+}
+
 
 def get_zuan(params):
     msg = requests.get(
-        "https://nmsl.shadiao.app/api.php",
-        params=params,
-        headers=headers
+        "https://nmsl.shadiao.app/api.php", params=params, headers=headers
     )
     return msg
 
-@bot.message_handler(commands=['random'])
+
+@bot.message_handler(commands=["random"])
 def random_zuan(message):
-    bot.send_chat_action(message.chat.id, 'typing')
-    msg = get_zuan({'level': 'min','lang': 'zh_cn'})
+    bot.send_chat_action(message.chat.id, "typing")
+    msg = get_zuan({"level": "min", "lang": "zh_cn"})
     bot.send_message(message.chat.id, msg.text)
 
-@bot.message_handler(commands=['fuck'])
+
+@bot.message_handler(commands=["fuck"])
 def random_zuan(message):
-    bot.send_chat_action(message.chat.id, 'typing')
-    msg = get_zuan({'lang': 'zh_cn'})
+    bot.send_chat_action(message.chat.id, "typing")
+    msg = get_zuan({"lang": "zh_cn"})
     bot.send_message(message.chat.id, msg.text)
 
-@bot.message_handler(commands=['ip'])
+
+@bot.message_handler(commands=["ip"])
 def get_ipinfo(message):
-    bot.send_chat_action(message.chat.id, 'typing')
-    api_url = 'https://ipinfo.io/json/'
+    bot.send_chat_action(message.chat.id, "typing")
+    api_url = "https://ipinfo.io/json/"
     try:
         ip = message.chat.text.split()[1]
-        p = re.compile("^((?:(2[0-4]\d)|(25[0-5])|([01]?\d\d?))\.){3}(?:(2[0-4]\d)|(255[0-5])|([01]?\d\d?))$")
+        p = re.compile(
+            "^((?:(2[0-4]\d)|(25[0-5])|([01]?\d\d?))\.){3}(?:(2[0-4]\d)|(255[0-5])|([01]?\d\d?))$"
+        )
         m = p.match(ip)
         if m:
-            params = {
-                'token': IP_TOKEN
-            }
+            params = {"token": IP_TOKEN}
             r = requests.get(api_url + m.group(), params=params).json()
+
             def info(data):
                 t = []
                 for k, v in data.items():
-                    if k=="asn":
+                    if k == "asn":
                         pass
                     else:
-                        t.append(k+":"+v)
-                for k, v in data['asn'].items():
-                    t.append(k+":"+v)
-                message = '\n'.join(t)
+                        t.append(k + ":" + v)
+                for k, v in data["asn"].items():
+                    t.append(k + ":" + v)
+                message = "\n".join(t)
                 return message
-            message = info(r)
-            bot.send_message(message.chat.id, message)
+
+            info = info(r)
+            bot.send_message(message.chat.id, info)
     except:
-        bot.send_message(message.chat.id, 'Require Valid IP')
+        bot.send_message(message.chat.id, "Require Valid IP")
+
 
 bot.remove_webhook()
 
 time.sleep(1)
 
-bot.set_webhook(
-    url=WEBHOOK_URL_BASE + WEBHOOK_URL_PATH
-)
+bot.set_webhook(url=WEBHOOK_URL_BASE + WEBHOOK_URL_PATH)
 
-app.run(
-    host='127.0.0.1',
-    port=5000
-)
+app.run(host="127.0.0.1", port=5000)
